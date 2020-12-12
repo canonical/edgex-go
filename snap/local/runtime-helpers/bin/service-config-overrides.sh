18c4dab4e2bafaecbb8aee3817f8012753291dd8
Tony Espy <espy@canonical.com>
2020-12-12 11:30:46 -0500

    [WIP] update service override script

--- snap/local/runtime-helpers/bin/service-config-overrides.sh
+++ snap/local/runtime-helpers/bin/service-config-overrides.sh
@@ -1,8 +1,21 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
-SERVICE_ENV="$SNAP_DATA/config/$@/res/$@.env"
+# convert cmdline to string array
+ARGV=($@)
+
+# grab binary path
+BINPATH="${ARGV[0]}"
+logger "edgex service override: BINPATH=$BINPATH"
+
+# binary name == service name/key
+SERVICE=$(basename "$BINPATH")
+logger "edgex service override: SERVICE=$SERVICE"
+
+SERVICE_ENV="$SNAP_DATA/config/$SERVICE/res/$SERVICE.env"
+logger "edgex service override: : SERVICE_ENV=$SERVICE_ENV"
 
 if [ -f "$SERVICE_ENV" ]; then
+    logger "edgex service override: : sourcing $SERVICE_ENV"
     source "$SERVICE_ENV"
 fi
 
